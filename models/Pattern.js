4219f1a348e56b54292b8b48363e151fe0c973a9
MyNameIsTaeYeong <imtaebari@gmail.com>
2021-03-05 16:13:43 +0900

    DB 재설계

--- models/Pattern.js
+++ models/Pattern.js
@@ -3,30 +3,35 @@ import mongoose from "mongoose";
 const PatternSchema = new mongoose.Schema({
     // 패턴 이름
     name: String,
-    // 패턴이 발생한 날들.
-    theDayGoodOccurred: [{
-        type: String
+    // 패턴 생성자
+    createdBy: String,
+    /* 
+        패턴의 값에 따라 
+        [0] : "good"일 때
+        [1] : "avg"일 때
+        [2] : "bad"일 때
+        연관된 패턴들 
+    */
+    patternsAssociatedWithValues : [{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: "RelatedPattern"
     }],
-    theDayAvgOccurred: [{
-        type: String
-    }],
-    theDayBadOccurred: [{
-        type: String
-    }],
-    // 패턴과 연관된 패턴들  
-    patternRelatedWithGood : [{
+
+    /*
+        날짜와 패턴의 값을 저장.
+        예) 20210303에 "good", 20210304에 "bad"가 발생했다면
+        {
+            "20210303" : "good",
+            "20210304" : "bad"
+        } 
+    */
+    theDayValueOccurred : {
         type: Map,
-        of: Number
-    }],
-    patternRelatedWithAvg : [{
-        type: Map,
-        of: Number
-    }],
-    patternRelatedWithBad : [{
-        type: Map,
-        of: Number
-    }],
-    createdBy: String
+        of: String,
+        default: {
+            "oh" : "TaeYeong is handsome"
+        }
+    }
 });
 
 const model = mongoose.model("Pattern", PatternSchema);
